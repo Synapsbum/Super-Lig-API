cb906ca720906b5afccf5e747b83081fa8602ac6
Synapsbum <emreemre2@icloud.com>
2024-10-06 06:33:48 +0300

    fsdfsd
    fsd

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 3005;
 // CollectAPI için API anahtarınızı buraya ekleyin
 const API_KEY = '5nNXjSikL7rF3MeyOZMr5P:2QXNBgyVdwJlGT7JIdG7vB'; // CollectAPI'den aldığınız anahtar
 
+// Süper Lig Haber: /superlig-haberler
+app.get('/superlig-haberler', async (req, res) => {
+    try {
+        const data = await getSuperligHaber();
+        res.json(data);
+    } catch (error) {
+        console.error('Hata:', error.message);
+        res.status(500).json({ error: 'Veri çekilemedi' });
+    }
+});
+
 // Süper Lig Puan Durumu Rotası: /superlig-puan-durumu
 app.get('/superlig-puan-durumu', async (req, res) => {
     try {
@@ -40,6 +51,24 @@ app.get('/superlig-son-maclar', async (req, res) => {
         res.status(500).json({ error: 'Veri çekilemedi' });
     }
 });
+
+// CollectAPI'den Süper Lig puan durumu verilerini çekme
+async function getSuperligHaber() {
+    try {
+        const response = await axios.get('https://api.collectapi.com/news/getNews?country=tr&tag=spor', {
+            headers: {
+                'authorization': `apikey ${API_KEY}`,
+                'content-type': 'application/json'
+            }
+        });
+
+        const standings = response.data.result;
+        return standings;
+    } catch (error) {
+        console.error('API Hatası:', error.message);
+        throw new Error('Veri çekilemedi.');
+    }
+}
 
 // CollectAPI'den Süper Lig puan durumu verilerini çekme
 async function getSuperligTable() {
